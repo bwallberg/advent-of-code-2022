7507385bf72b3d1d085f6f0d4909e568ddc26a54
Benjamin Wallberg <me@bwallberg.com>
2022-12-03 22:28:17 +0100

    day_3: requires nightly

---
+++ src/day_3.rs
@@ -0,0 +1,83 @@
+#![feature(iter_next_chunk)]
+
+use crate::advent::read_lines;
+
+fn get_duplicate(rucksack: &String) -> Option<char> {
+  let (first, second) = rucksack.split_at(rucksack.len() / 2);
+  first.chars().find(|item| second.contains(*item))
+}
+
+fn get_groups(input: Vec<String>) -> Vec<char> {
+  let mut iter = input.into_iter();
+
+  let mut groups: Vec<char> = Vec::new();
+  while let Ok(chunk) = iter.next_chunk::<3>() {
+    let group_item = chunk[0]
+      .chars()
+      .find(|item| chunk[1].contains(*item) && chunk[2].contains(*item))
+      .unwrap();
+    groups.push(group_item)
+  }
+
+  groups
+}
+
+fn get_item_priority(item: char) -> i32 {
+  let mut priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars();
+  (priority.position(|letter| letter == item).unwrap() + 1)
+    .try_into()
+    .unwrap()
+}
+
+pub fn step_1() {
+  let sum: i32 = read_lines("3")
+    .unwrap()
+    .into_iter()
+    .map(String::from)
+    .map(|rucksack| get_duplicate(&rucksack).unwrap())
+    .map(get_item_priority)
+    .sum();
+
+  println!("Solution Step 1: {}", sum);
+}
+
+#[test]
+pub fn step_1_test() {
+  let sum: i32 = "vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw"
+    .lines()
+    .map(String::from)
+    .map(|rucksack| get_duplicate(&rucksack).unwrap())
+    .map(get_item_priority)
+    .sum();
+
+  assert_eq!(sum, 157)
+}
+
+pub fn step_2() {
+  let input = read_lines("3").unwrap();
+  let sum: i32 = get_groups(input).into_iter().map(get_item_priority).sum();
+
+  println!("Solution Step 2: {}", sum);
+}
+
+#[test]
+pub fn step_2_test() {
+  let input: Vec<String> = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+    .lines()
+    .map(String::from)
+    .collect();
+
+  let sum: i32 = get_groups(input).into_iter().map(get_item_priority).sum();
+
+  assert_eq!(sum, 70)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+  #![feature(iter_next_chunk)]
+
+
 use std::env;
 
 mod advent;
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
   if args.len() < 2 {
-    day_2::step_1();
-    day_2::step_2();
+    day_3::step_1();
+    day_3::step_2();
   } else {
     match args[1].as_str() {
       "1" => {
@@ -19,6 +23,10 @@ fn main() {
       "2" => {
         day_2::step_1();
         day_2::step_2();
+      },
+      "3" => {
+        day_3::step_1();
+        day_3::step_2();
       }
       &_ => println!("Day {} solution not available", args[1].as_str()),
     }
