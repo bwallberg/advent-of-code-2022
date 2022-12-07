541e78eb3a83efe2678a166e0fbc14cbefdb7c65
Benjamin Wallberg <me@bwallberg.com>
2022-12-07 14:17:49 +0100

    day_6: a bit optimized

--- src/day_6.rs
+++ src/day_6.rs
@@ -10,16 +10,16 @@ pub fn main() {
 
 fn step_1(input: &str) -> i32 {
   let mut marker: String = String::new();
-  let mut marker_index: i32 = -1;
-  input.chars().enumerate().for_each(|(i, c)| {
-    marker.push(c);
-    if marker.chars().into_iter().unique().count() == 4 {
-      marker_index = (i + 1) as i32;
-    } else if marker.len() == 4 {
-      marker.remove(0);
-    }
-  });
-  marker_index
+  let (i, _) = input.chars().enumerate().find(|(_, c)| {
+      marker.push(*c);
+      if marker.chars().into_iter().unique().count() == 4 {
+        return true
+      } else if marker.len() == 4 {
+        marker.remove(0);
+      }
+      false
+  }).unwrap();
+  (i + 1) as i32
 }
 
 #[test]
@@ -31,19 +31,16 @@ fn step_1_test() {
 
 fn step_2(input: &str) -> i32 {
   let mut marker: String = String::new();
-  let mut marker_index: i32 = -1;
-  input.chars().enumerate().for_each(|(i, c)| {
-    // should obviusly not continue iterting when marker_index is found here...
-    if marker_index == -1 {
-      marker.push(c);
+  let (i, _) = input.chars().enumerate().find(|(_, c)| {
+      marker.push(*c);
       if marker.chars().into_iter().unique().count() == 14 {
-        marker_index = (i + 1) as i32;
+        return true
       } else if marker.len() == 14 {
         marker.remove(0);
       }
-    }
-  });
-  marker_index
+      false
+  }).unwrap();
+  (i + 1) as i32
 }
 
 #[test]
